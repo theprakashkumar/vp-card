2f83e4b80c4978690d355fe57a1628ec21bb4c0d
theprakashkumar <prakashcodes@outlook.com>
2022-12-22 03:42:52 +0530

    style MainHeader

--- src/component/Main/MainHeader.jsx
+++ src/component/Main/MainHeader.jsx
@@ -1,6 +1,8 @@
+import "./MainHeader.css";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { DataContext } from "../../context/DataProvider";
+import { BsGrid, BsViewStacked } from "react-icons/bs";
 
 const MainHeader = () => {
     const { currentTab, setTab } = useContext(DataContext);
@@ -8,16 +10,42 @@ const MainHeader = () => {
         setTab(data);
     };
     return (
-        <div>
-            <Link to="/your" onClick={() => onClickHandler("your")}>
-                Your
-            </Link>
-            <Link to="/" onClick={() => onClickHandler("all")}>
-                All
-            </Link>
-            <Link to="/blocked" onClick={() => onClickHandler("your")}>
-                Blocked
-            </Link>
+        <div className="main-header">
+            <div className="main-header_left">
+                <Link
+                    to="/your"
+                    className={`main-header_link ${
+                        currentTab === "your" ? "main-header_link-active" : ""
+                    }`}
+                    onClick={() => onClickHandler("your")}
+                >
+                    Your
+                </Link>
+                <Link
+                    to="/"
+                    className={`main-header_link ${
+                        currentTab === "all" ? "main-header_link-active" : ""
+                    }`}
+                    onClick={() => onClickHandler("all")}
+                >
+                    All
+                </Link>
+                <Link
+                    to="/blocked"
+                    className={`main-header_link ${
+                        currentTab === "blocked"
+                            ? "main-header_link-active"
+                            : ""
+                    }`}
+                    onClick={() => onClickHandler("blocked")}
+                >
+                    Blocked
+                </Link>
+            </div>
+            <div className="main-header_right">
+                <BsGrid className="main-header_right_icon" />
+                <BsViewStacked className="main-header_right_icon" />
+            </div>
         </div>
     );
 };

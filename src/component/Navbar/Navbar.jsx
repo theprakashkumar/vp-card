eed2af23c0ed7bbae2b8eb3bc168ec6e1de2472f
theprakashkumar <prakashcodes@outlook.com>
2022-12-21 19:08:54 +0530

    add navbar

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+import { FiVideo, FiPlus } from "react-icons/fi";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar_left">
+                <h1 className="navbar_left_brand">Virtual cards</h1>
+                <a href="#" className="navbar_left_link">
+                    <FiVideo />
+                    <span>Learn more</span>
+                </a>
+            </div>
+            <div className="navbar_right">
+                <button className="navbar_right_button">
+                    <FiPlus />
+                    <span className="navbar_right_button_text">
+                        Virtual card
+                    </span>
+                </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
